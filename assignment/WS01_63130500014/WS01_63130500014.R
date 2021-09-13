9cd5918657a134179f22969e7014c136b1a06815
Chanunporn Pongsri <chanunporn.pongsri@mail.kmutt.ac.th>
2021-09-13 16:49:49 +0700

    014WS_01

---
+++ assignment/WS01_63130500014/WS01_63130500014.R
@@ -0,0 +1,10 @@
+#0.Load dataset
+sat_score <- read_csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/SAT_original.csv")
+spec(sat_score)
+
+#1.Observe and Answer
+View(sat_score)
+
+#1.1 How many Observation
+glimpse(sat_score)
+#485
