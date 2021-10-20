87b2ee4b49ea8eb4407ad3a125e1b2fee68af43b
Narawich K <67256899+catintheboxx@users.noreply.github.com>
2021-10-21 00:50:18 +0700

    Update HW04_63130500065.R

--- assignment/HW04_63130500065/HW04_63130500065.R
+++ assignment/HW04_63130500065/HW04_63130500065.R
@@ -8,25 +8,25 @@ library(forcats)
 
 cs270 <- read.csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/prog_book.csv")
 
-#2.1
+#3.1
 cs270 %>% glimpse()
 
-#2.2
+#3.2
 cs270 %>% group_by(Type) %>% select(Rating) %>% summarise(avg = mean(Rating, na.rm = TRUE)) %>% arrange(desc(avg))
 
-#2.3
+#3.3
 cs270 %>% group_by(Book_title) %>% select(Price) %>% summarise(max_price = max(Price))%>% arrange(desc(max_price)) %>% head(1)
 cs270 %>% group_by(Book_title) %>% select(Price) %>% summarise(min_price = min(Price))%>% arrange((min_price)) %>% head(1)
 
-#2.4
+#3.4
 cs270$Rating %>% mean()
 cs270$Rating %>% max()
 cs270$Rating %>% min()
 
-#2.5
+#3.5
 cs270 %>% select(Type) %>% table()
 
-#2.6
+#3.6
 str_subset(cs270$Book_title, "Programming")
 
 ## ggplot
